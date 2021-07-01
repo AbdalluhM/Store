bab13c9d235d8bd706b56b4ef19bded12a5d6a7c
AbdalluhM <77342166+AbdalluhM@users.noreply.github.com>
2021-07-01 10:50:25 +0200

    edit project

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\API\BaseController;
 
 class AuthController extends BaseController
 {
